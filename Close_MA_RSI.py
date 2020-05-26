8fd4dfb0a3ae9a9595dc357cc2a7e23d432844b3
Hursh Karkhanis <pullupj7@gmail.com>
2020-05-26 15:30:59 -0700

    added comments, removed print, & made dates recent

--- Close_MA_RSI.py
+++ Close_MA_RSI.py
@@ -9,34 +9,34 @@ import numpy as np
 import pandas_datareader.data as web 
 
 
-start = dt.datetime(2019, 1, 1);
-end = dt.datetime(2019, 7, 28);
+#establishing start and end dates
+start = dt.datetime(2020, 1, 1)
+end = dt.datetime(2020, 5, 20)
 
  
-df = web.DataReader('AMZN', 'yahoo', start, end);
+#reading yahoo finance data on AMZN
+df = web.DataReader('WORK', 'yahoo', start, end)
 
 
+#creating columns for moving averages and changes
 df['CHANGE'] = ( df['Close'] - df['Open'] )
 
-df['15ma'] = df['Adj Close'].rolling(window = 15, min_periods = 0).mean();
-df['20ma'] = df['Adj Close'].rolling(window = 20, min_periods = 0).mean();
-df['30ma'] = df['Adj Close'].rolling(window = 30, min_periods = 0).mean();
-df['50ma'] = df['Adj Close'].rolling(window = 50, min_periods = 0).mean();
+df['15ma'] = df['Adj Close'].rolling(window = 15, min_periods = 0).mean()
+df['20ma'] = df['Adj Close'].rolling(window = 20, min_periods = 0).mean()
+df['30ma'] = df['Adj Close'].rolling(window = 30, min_periods = 0).mean()
+df['50ma'] = df['Adj Close'].rolling(window = 50, min_periods = 0).mean()
 
 
-
-df['100ma'] = df['Adj Close'].rolling(window = 100, min_periods = 0).mean();
-
-
-
+df['100ma'] = df['Adj Close'].rolling(window = 100, min_periods = 0).mean()
 
 df['gain'] = np.where(df['CHANGE']>=0, df['CHANGE'], 0)
 df['loss'] = np.where(df['CHANGE']<=0, df['CHANGE'], 0)
 
 
-df['14Pgain'] = df['gain'].rolling(window = 14, min_periods = 0).mean();
-df['14Ploss'] = df['loss'].rolling(window = 14, min_periods = 0).mean();
+df['14Pgain'] = df['gain'].rolling(window = 14, min_periods = 0).mean()
+df['14Ploss'] = df['loss'].rolling(window = 14, min_periods = 0).mean()
 
+#the to steps to calculating RSI
 df['step one'] = 100-(100/(1+((df['14Pgain'])/(df['14Ploss']))))
 
 df['step two'] = 100-(100/(1+((df['14Pgain']*13+df['gain'])/(df['14Ploss']*13+df['loss']))))
@@ -44,20 +44,19 @@ df['step two'] = 100-(100/(1+((df['14Pgain']*13+df['gain'])/(df['14Ploss']*13+df
 df['Stochastic RSI'] =  (df['step two'] - df['step two'].rolling(window = 14, min_periods = 0).min() ) / (df['step two'].rolling(window = 14, min_periods = 0).max() - df['step two'].rolling(window = 14, min_periods = 0).min())
 
 
-print df.tail(10)
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan = 5, colspan = 1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan = 1, colspan = 1, sharex = ax1)
 
 
-ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan = 5, colspan = 1);
-ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan = 1, colspan = 1, sharex = ax1);
-
-
+#plotting closing price, moving averages on top plot
+#plotting RSI on botton plot
 ax1.plot(df.index, df['Adj Close'])
 ax1.plot(df.index, df['15ma'])
 ax1.plot(df.index, df['20ma'])
 ax1.plot(df.index, df['30ma'])
 ax1.plot(df.index, df['50ma'])
 ax1.plot(df.index, df['100ma'])
-ax2.plot(df.index, df['Stochastic RSI']); 
+ax2.plot(df.index, df['Stochastic RSI']) 
 
 
 
